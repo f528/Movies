4c433f78feb354ecb3c9e6213cfb2b0f08dfa7eb
fouzi <faouzi_ee@hotmail.it>
2023-12-03 18:29:05 +0100

    scrollbar Pregess created

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 
+/***===progress-bar======= */
+let scrollPrecentage = () => {
+    let scrollProgress = document.getElementById('progress');
+    let progressValue = document.getElementById('progress-value');
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round(pos * 100/calcHeight);
+    scrollProgress.style.background = `conic-gradient(#e70634 ${scrollValue}%,#2b3f38 ${scrollValue}%)`;
 
+
+}
+window.onscroll = scrollPrecentage;
+window.onload = scrollPrecentage;
 
 
 var swiper = new Swiper(".mySwiper", {
@@ -40,4 +52,14 @@ var swiper = new Swiper(".mySwiper", {
             spaceBetween: 30,
         },
     },
+});
+
+/**menu-btn-fixed-when-scroll----------- */ 
+$(window).scroll(function(){
+    if($(document).scrollTop()>20){
+        $('.navigation').addClass('fix-icon');
+    }
+    else  {
+        $('.navigation').removeClass('fix-icon');
+    }
 });
