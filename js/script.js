d7d5b49f35e4a4a3dfe2c5b3805db883cc2f49be
fouzi <faouzi_ee@hotmail.it>
2023-12-13 19:15:53 +0100

    trailer movie section created

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let scrollPrecentage = () => {
     let progressValue = document.getElementById('progress-value');
     let pos = document.documentElement.scrollTop;
     let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let scrollValue = Math.round(pos * 100/calcHeight);
+    let scrollValue = Math.round(pos * 100 / calcHeight);
     scrollProgress.style.background = `conic-gradient(#e70634  ${scrollValue}%,  #2b3f38  ${scrollValue}%)`;
 
 
@@ -15,26 +15,26 @@ window.onload = scrollPrecentage;
 
 
 var swiper = new Swiper(".mySwiper", {
- 
-  
-        lazy: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-    
+
+
+    lazy: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
     slidesPerView: 3,
     spaceBetween: 10,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
-    autoplay:{
-        delay:5000,
+    autoplay: {
+        delay: 5000,
         disableOnInteraction: false,
 
     },
@@ -54,12 +54,35 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
-/**menu-btn-fixed-when-scroll----------- */ 
-$(window).scroll(function(){
-    if($(document).scrollTop()>20){
+/**menu-btn-fixed-when-scroll----------- */
+$(window).scroll(function () {
+    if ($(document).scrollTop() > 20) {
         $('.navigation').addClass('fix-icon');
     }
-    else  {
+    else {
         $('.navigation').removeClass('fix-icon');
     }
 });
+
+// popup-open---===============================================================
+$(document).on('click', '.play-btn a', function () {
+    $('.play').addClass('active-popup');
+
+});
+// popup-close---============================================================
+$(document).on('click', '.close-movie', function () {
+    $('.play').removeClass('active-popup');
+
+})
+
+//=======auto-play-whene-popup-open============================>
+$('.play-btn a').click(function () {
+    $('#m-movie')[0].play();
+ 
+});
+//==Close-video-where-popup-close======================================>
+$('.close-movie').click(function () {
+    $('#m-movie')[0].pause();
+
+});
+
